6768c00586f9272d847339fd4ce5452995509ce5
tengkurizki13 <trizkidinelvipratama@gmail.com>
2023-09-14 13:21:28 +0700

    feat: add skeleton of server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const env = require("dotenv").config();
+const express = require("express");
+const router = require("./routes");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
